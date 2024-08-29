1a0615f83ebfb05251aee53faaf99d7ba32ecb0e
hndko <hd.doko22@gmail.com>
2024-08-29 14:49:14 +0700

    done

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -16,3 +17,32 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('admin', function () {
+    return '<h1>Hello Admin</h1>';
+})->middleware(['auth', 'verified', 'role:admin']);
+
+Route::get('penulis', function () {
+    return '<h1>Hello Penulis</h1>';
+})->middleware(['auth', 'verified', 'role:penulis|admin']);
+
+// Route::get('tulisan', function () {
+//     return '<h1>Hello Tulisan</h1>';
+// })->middleware(['auth', 'verified', 'permission:lihat-tulisan']);
+
+Route::get('tulisan', function () {
+    // return '<h1>Hello Tulisan</h1>';
+    return view('tulisan');
+})->middleware(['auth', 'verified', 'role_or_permission:lihat-tulisan|admin'])->name('tulisan');
+
+require __DIR__ . '/auth.php';
